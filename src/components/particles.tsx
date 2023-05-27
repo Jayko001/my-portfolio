f830c0bb551054c7c0f569428e8d417c44de84d7
Jay Kothari <jaykothari.d@gmail.com>
2023-05-27 04:50:02 -0400

    ts bug fix attempt -2

--- src/components/particles.tsx
+++ src/components/particles.tsx
@@ -3,15 +3,15 @@ import { loadFull } from "tsparticles";
 
 const ParticleBackground = () => {
     const particlesInit = async (main:any) => { 
-		console.log(main); 
+	// 	console.log(main); 
 		// you can initialize the tsParticles instance (main) here, adding custom shapes or presets 
 		// this loads the tsparticles package bundle, it's the easiest method for getting everything ready 
 		// starting from v2 you can add only the features you need reducing the bundle size 
 		await loadFull(main); 
 		}; 
-		// const particlesLoaded = (container:any) => { 
-		// 	console.log(container); 
-		// }; 
+	// 	const particlesLoaded = (container:any) => { 
+	// 		console.log(container); 
+	// 	}; 
     return (
         <Particles 
         id="tsparticles" 
