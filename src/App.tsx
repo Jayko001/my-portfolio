ac1550d20f0d5034e067cdd0d82d7fb18e1d0fbe
Jay Kothari <jaykothari.d@gmail.com>
2023-05-27 01:34:35 -0400

    test commit -2

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,27 @@ function App() {
 
   return (
     <>
-      hii, my name is jay and this is my first vite react app
-      this is commit 2
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+      this is a test
     </>
   )
 }
