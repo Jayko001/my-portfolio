b13bba71d0a7b9b9294464a4cdeb269aedf07f9a
Jay Kothari <jaykothari.d@gmail.com>
2023-06-19 01:34:06 -0400

    contact me page v.1

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,7 +1,75 @@
-export default function Contact (){
-    return (
-    <h1 className="z-10 text-4xl text-transparent bg-white cursor-default text-edge-outline font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text ">
-        Contact
-    </h1>
-    )
-}
+import React, { useEffect } from 'react';
+import '../assets/ContactMe.css';
+
+const ContactMe: React.FC = () => {
+
+  const rand = (min: number, max: number) => Math.floor(Math.random() * (max - min + 1)) + min;
+
+  const enhance = (id: string) => {
+    const element = document.getElementById(id),
+          text = element?.innerText.split("");
+    
+    element!.innerText = "";
+    
+    text!.forEach((value, index) => {
+      const outer = document.createElement("span");
+      
+      outer.className = "outer";
+      
+      const inner = document.createElement("span");
+      
+      inner.className = "inner";
+      
+      inner.style.animationDelay = `${rand(-5000, 0)}ms`;
+      
+      const letter = document.createElement("span");
+      
+      letter.className = "letter";
+      
+      letter.innerText = value;
+      
+      letter.style.animationDelay = `${index * 1000 }ms`;
+      
+      inner.appendChild(letter);    
+      
+      outer.appendChild(inner);    
+      
+      element!.appendChild(outer);
+    });
+  }
+
+  useEffect(() => {
+    enhance("channel-link");
+  }, []);
+
+  return (
+    <div id="text">
+      <div className="line">
+        <p className="word">A</p>
+        <p className="word">Person</p>
+      </div>
+
+      <div className="line">
+        <p className="word">Blog</p>
+        <p className="word">&</p>
+      </div>
+
+      <div className="line">
+        <p className="word">student</p>
+      </div>
+      
+      <div className="line">
+        <a 
+           id="channel-link" 
+           href="https://jayko.me" 
+           target="_blank" 
+           className="word fancy"
+        >
+          @Jay Kothari
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default ContactMe;
