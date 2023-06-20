021c8f344a9ef12806b26c4f186330459f19b95d
Jay Kothari <jaykothari.d@gmail.com>
2023-06-20 01:20:36 -0400

    blog page

--- src/components/BlogCard.tsx
+++ src/components/BlogCard.tsx
@@ -11,19 +11,22 @@ type Props = {
 
 const BlogCard = ({id, title, description, date, link, image }: Props) => {
     return (
-        <div className="flex flex-col justify-between p-4 bg-white rounded-lg shadow-lg">
+        <div className="flex flex-col bg-black text-white p-4 rounded-[24px] shadow-lg border-white border-2">
             <div>
-                <img
-                    className="object-cover w-full h-48 rounded-t-lg"
-                    src= {image}
-                    alt="Article"
-                />
-                <h3 className="text-2xl font-semibold text-gray-800">{title}</h3>
-                <p className="mt-2 text-gray-600">{description}</p>
+                <div className="overflow-hidden">
+                    <img
+                        className="object-cover w-full h-48 rounded-t-lg hover:scale-110 transition duration-500 cursor-pointer"
+                        src= {image}
+                        alt="Article"
+                    />
+                </div>
+                <h3 className="text-2xl font-semibold pt-2 pb-2">{title}</h3>
+                <hr className="h-1 bg-black pb-2"/>
+                <p className="mt-2 text-gray-300">{description}</p>
             </div>
             <div className="flex items-center justify-between mt-4">
                 <Link to={link} className="px-4 py-2 text-sm font-medium text-white bg-gray-800 rounded hover:bg-gray-700"> Read </Link>
-                <p className="text-sm font-medium text-gray-600">{date} {id}</p>
+                <p className="text-sm font-medium text-gray-500">{date} {id}</p>
             </div>
         </div>
     );
--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -1,18 +1,33 @@
 // import React from "react"
 import BlogCard from "../components/BlogCard"
+import blog1 from "../assets/logo.jpeg"
+import blog2 from "../assets/test1.png"
 
 export default function Blog (){
     return (
         <>
-            <h1> This is my blog </h1>
-            <p> I will share my thoughts and ideas here</p>
+            <div className="bg-black">
+                <div className=" m-10 mr-10 h-72 rounded-[24px] bg-gradient-to-r from-yellow-500 to-amber-500">
+                    <div className="flex flex-row relative">
+                        <div className="text-7xl text-white absolute left-10 top-36">
+                            Jay Kothari -
+                        </div>
+                        <div className="absolute end-10 top-32">
+                            <img src="https://github.com/Jayko001.png" className="w-32 rounded-full"/>
+                        </div>
+                        
+                    </div>
+                </div>
 
-            <div className="w-full">
-                <BlogCard id={1} title="Why I am awesome" description="This is an article going over why I am awesome" date="24th April 2023" link={`/blog/${1}`} image='src/assets/logo.jpeg' />
-                <BlogCard id={2} title="Why I am the best" description="This is an article going over why I am the best" date="30th April 2023" link={`/blog/${2}`} image='src/assets/test1.png' />
-
-            </div> 
-            
+                <div className="flex md:flex-row flex-col"> 
+                    <div className="basis-1/3 m-5">
+                        <BlogCard id={1} title="Why I am awesome" description="This is an article going over why I am awesome" date="24th April 2023" link={`/blog/${1}`} image={blog1} />
+                    </div>
+                    <div className="basis-1/3 m-5">
+                        <BlogCard id={2} title="Why I am the best" description="This is an article going over why I am the best" date="30th April 2023" link={`/blog/${2}`} image={blog2} />
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
