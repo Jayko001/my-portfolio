f3299695130d05ab11a9241c4fde16bb65fee881
Jay Kothari <jaykothari.d@gmail.com>
2023-07-03 14:27:06 -0400

    ts bug fixes

--- src/components/LoadingScreen.tsx
+++ src/components/LoadingScreen.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import  { useState } from 'react';
 import Logo from "../assets/logo.jpeg"
 
 const funFacts = [
@@ -10,7 +10,7 @@ const funFacts = [
 ];
 
 const LoadingScreen = () => {
-    const [fact, setFact] = useState(funFacts[Math.floor(Math.random() * funFacts.length)]);
+    const [fact, _setFact] = useState(funFacts[Math.floor(Math.random() * funFacts.length)]);
    
     return (
         <>
